9b1d8a8a762f374c2e0d00f7d69180dcf9b65c70
fruitgummi7 <firyuza611@gmail.com>
2026-03-04 20:22:24 +0300

    Практикуемся в реализации gRPC API клиентов

--- tools/fakers.py
+++ tools/fakers.py
@@ -1,5 +1,5 @@
 import time
-
+from google.protobuf.internal.enum_type_wrapper import EnumTypeWrapper
 from faker import Faker
 from faker.providers.python import TEnum
 
@@ -111,6 +111,15 @@ class Fake:
         :return: Сумма от 1 до 1000.
         """
         return self.float(1, 1000)
+
+    def proto_enum(self, value: EnumTypeWrapper) -> int:
+        """
+        Выбирает случайное значение из proto enum-типа.
+
+        :param value: Proto enum-класс для генерации значения.
+        :return: Случайное значение из перечисления.
+        """
+        return self.faker.random_element(value.values())
 
 
 # Создаем экземпляр класса Fake с использованием Faker
