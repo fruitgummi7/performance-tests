7e65c49f45d740a56270f999edc757fa5167187e
fruitgummi7 <firyuza611@gmail.com>
2026-05-05 21:53:44 +0300

    Настройки нагрузочных тестов с Pydantic

---
+++ pydantic_settings_basics.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel, EmailStr, SecretStr, HttpUrl
+
+
+class TestUser(BaseModel):
+    email: EmailStr
+    password: SecretStr
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env.basics",
+        env_file_encoding="utf-8",
+        env_nested_delimiter="."
+    )
+
+    base_url: HttpUrl
+    test_user: TestUser
+
+
+print(Settings())
---
+++ tools/config/grpc.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class GRPCClientConfig(BaseModel):
+    port: int
+    host: str
+
+    @property
+    def client_url(self) -> str:
+        return f"{self.host}:{self.port}"
